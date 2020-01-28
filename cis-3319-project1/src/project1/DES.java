ab210576da25b592140c912f4fcf548d5de15625
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-28 17:02:43 -0500

    update

--- cis-3319-project1/src/project1/DES.java
+++ cis-3319-project1/src/project1/DES.java
@@ -2,12 +2,14 @@ package project1;
 
 import javax.crypto.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
@@ -16,72 +18,44 @@ public class DES {
 
 
     public static void main(String[] args) {
-        //works!
-        Cipher("hello world",0);
+        byte[] final_word_in_byte = Cipher("hello world",0,null);
+        System.out.println(Arrays.toString(final_word_in_byte));
 
-        //not works!!!
-        Cipher("[B@762efe5d",1);
-
+        byte[] original = Cipher(null,1,final_word_in_byte);
+        System.out.println(Arrays.toString(original));
     }
 
     //0 - > encrypt
     // 1 - > decrypt
-    public static void Cipher(String words, int mode){
+    public static byte[] Cipher(String words, int mode,byte[] raw){
         try{
 
-            /*
-            Just ignore this part
-            */
-            //Basic setup ---------------------------------->
-            KeyGenerator kg = KeyGenerator.getInstance("DES");
-            SecretKey mykey = kg.generateKey();
-
-            Cipher Ecipher = Cipher.getInstance("DES");;
-            Cipher Dcipher =  Cipher.getInstance("DES");;;
-
-            //Basic setup Finished-------------------------->
-
-
-            if (mode == 0) {
-                // Convert the worlds in to bytes mode  ---------------------------------------->
-                byte[] words_in_bytes  = words.getBytes("UTF8");
-                // Print out the original words
-                System.out.println("Original words: " + new String(words_in_bytes) + "\n");
-                // Print out the words in bytes
-                System.out.println("\nwords -> Bytes : " + words_in_bytes);
-                // Convert the worlds in to bytes mode Finished -------------------------------->
-
-
-                //-------------------------------Encryption---------------------------------------->
-                // Encrypt the words
-                Ecipher.init(Cipher.ENCRYPT_MODE,mykey);
-                byte[] outcome_from_cipher = Ecipher.doFinal(words_in_bytes);
-                System.out.println("After Encrypt : " + outcome_from_cipher);
-                System.out.println("New String : " + new String(outcome_from_cipher) + "\n");
-                //-------------------------------Encryption Finished------------------------------->
-
+            if (mode == 0)
+            {
+                KeyGenerator kg = KeyGenerator.getInstance("DES");
+                SecretKey mykey = kg.generateKey();
+                Cipher Encipher = Cipher.getInstance("DES");
+                byte[] words_in_bytes  = words.getBytes(StandardCharsets.UTF_8);
+                Encipher.init(Cipher.ENCRYPT_MODE,mykey);
+                return Encipher.doFinal(words_in_bytes);
             }
+
+
             else if(mode == 1){
-                //-------------------------------Decryption---------------------------------------->
-                Dcipher.init(Cipher.DECRYPT_MODE,mykey);
-
-                byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(words);
-                byte[] utf8 = Dcipher.doFinal(dec);
-
-                byte[] to_words = Dcipher.doFinal(utf8);
-                System.out.println("Decrypt: " + to_words);
-
-                //-------------------------------Decryption Finished------------------------------------>
-
+                KeyGenerator kg = KeyGenerator.getInstance("DES");
+                SecretKey second_key = kg.generateKey();
+                Cipher Decipher =  Cipher.getInstance("DES");
+                Decipher.init(Cipher.DECRYPT_MODE,second_key);
+                return Decipher.doFinal(raw);
             }
 
 
 
 
-        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException | IOException e) {
+        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException e) {
             e.printStackTrace();
         }
-
+        return null;
     }
 
 
