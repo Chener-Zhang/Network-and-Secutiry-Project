1df0984976da571ce9dc9096d7aaca25e9122130
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-28 22:37:15 -0500

    up

--- cis-3319-project1/src/project1/Client.java
+++ cis-3319-project1/src/project1/Client.java
@@ -4,7 +4,18 @@ package project1;
 import java.net.*;
 import java.io.*;
 
-public class Client
-{
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("127.0.0.1",5000);
+            
+
+            System.out.println();
+        } catch (UnknownHostException e) {
+            System.out.println("IP not found" + e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 
 }
