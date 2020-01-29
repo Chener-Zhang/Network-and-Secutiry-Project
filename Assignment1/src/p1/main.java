6f9313424f0953bd135789aef7440b38d0d6f2d4
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-28 23:16:39 -0500

    update

---
+++ Assignment1/src/p1/main.java
@@ -0,0 +1,18 @@
+package p1;
+
+
+class main {
+
+    public static void main(String[] args) {
+
+
+        /*
+
+        DES cipher_test = new DES();
+        byte[] encrypt = cipher_test.Encrypt("this is the class 3319\n");
+        byte[] orignal = cipher_test.Decrypt(encrypt);
+        */
+
+
+    }
+}
