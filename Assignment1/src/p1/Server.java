32f31855c820a95736792513bb7207bec3bb7442
tuh12085 <tuh12085@temple.edu>
2020-01-30 10:19:42 -0500

    change

--- Assignment1/src/p1/Server.java
+++ Assignment1/src/p1/Server.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Server {
 
-    // constructor with port
+    // constructor
     public Server(int port) throws ClassNotFoundException {
         // starts server and waits for a connection
         try {
@@ -20,50 +20,46 @@ public class Server {
             System.out.println("Client accepted");
 
             // takes input from the client socket
-
-            DataInputStream in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
+            DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
             String line = "";
-            DES cipher_test = new DES();
             // reads message from client until "Over" is sent
-            // Edit here for cipher project
+
+            //implement my cipher class
+            DES cipher_test = new DES();
             while (!line.equals("Over")) {
                 try {
 
                     line = in.readUTF();
-                    //System.out.println(line);
-
                     DataInputStream dIn = new DataInputStream(socket.getInputStream());
                     int length = dIn.readInt();    
-                    byte[] message = new byte[length];// read length of incoming message
+                    byte[] message = new byte[length];
                         if(length>0) {
                             dIn.readFully(message, 0, message.length); // read the message
                         }
 
-                    System.out.println("The message you received is :" + Arrays.toString(message));
-                    System.out.println("In String is :" + Arrays.toString(message));
+                    //System.out.println("The message you received is :" + Arrays.toString(message));
+                    //System.out.println("In String is :" + Arrays.toString(message));
 
 
                     //get the key object
                     InputStream inputStream = socket.getInputStream();
                     ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-                    
+
                     SecretKey key = (SecretKey) objectInputStream.readObject();
-                    System.out.println("The key you received : \n" + Arrays.toString(key.getEncoded()));
-                    System.out.println(Arrays.toString(key.getEncoded()));
+                    
+                    //System.out.println("The key you received : \n" + Arrays.toString(key.getEncoded()));
+                    //System.out.println(Arrays.toString(key.getEncoded()));
 
                     System.out.println("You got :");
                     cipher_test.Decrypt(message, key);
-                    
-                    
+
                 } catch (IOException i) {
                     System.out.println(i.toString());
                 }
             }
 
             System.out.println("Closing connection");
-
             // close connection
             socket.close();
             in.close();
