7a65008fdbe6d29c76ca3d6140a8a3c8ed407909
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-02-18 09:36:34 -0500

    + combination

--- Assignment2/src/project/Code_Runner.java
+++ Assignment2/src/project/Code_Runner.java
@@ -3,14 +3,32 @@ package project;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
+import java.util.Scanner;
 
 public class Code_Runner {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
 
+    public static void main(String[] args) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+        // Init the function
         HMAC mac = new HMAC();
-        byte[] item = mac.Calculate_HMAC("hello world", "hey");
+        Scanner scanner = new Scanner(System.in);
+
+        //print out the system message
+        System.out.println("Enter they message:");
+        String message = scanner.nextLine();
+        System.out.println("Enter the key:");
+        String key = scanner.nextLine();
+
+        //HMAC conventer
+        byte[] item = mac.Calculate_HMAC(message, key);
         System.out.println(mac.StringToHex(item));
+
+        //DES key generator
+        DES_key_generator_from_p1 generator = new DES_key_generator_from_p1();
+        String DES_key = generator.keyToString();
+        System.out.println(DES_key);
+
+
     }
 
 }
