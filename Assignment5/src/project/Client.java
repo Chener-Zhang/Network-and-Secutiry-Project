44b319d043627f35b7eccc3ab14cbb1948bb8da6
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-06 11:07:42 -0400

    Update Client.java

--- Assignment5/src/project/Client.java
+++ Assignment5/src/project/Client.java
@@ -16,18 +16,26 @@ public class Client {
     public Client() {
     }
 
-    public static void connection_start() throws IOException {
-            Socket socket = new Socket("127.0.0.1",5000);
-            if(socket.isConnected()){
-                System.out.println("connection success");
-            }else{
-                System.out.println("connection fail");
-            }
-            socket.close();
-    }
-
     public static void main(String[] args) throws IOException {
-        connection_start();
+        connection_start(5000);
     }
 
+
+    public static void connection_start(int port) throws IOException {
+        //create a socket
+        Socket socket = new Socket("127.0.0.1", port);
+
+        //check if connection success
+        if (socket.isConnected()) {
+            System.out.println("connection success");
+
+            
+        } else {
+            System.out.println("connection fail");
+        }
+
+        //close the connection
+        socket.close();
+
+    }
 }
