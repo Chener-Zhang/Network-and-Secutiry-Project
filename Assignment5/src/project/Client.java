cd921f81ce08eff625150b404b1f95e0ed71833a
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-07 17:28:59 -0400

    Server finished

--- Assignment5/src/project/Client.java
+++ Assignment5/src/project/Client.java
@@ -25,11 +25,41 @@ public class Client {
         //client.AS_connection_start(5000);
 
         //2
-        client.TGS_connection_start(5000);
+        //client.TGS_connection_start(5000);
 
+        //3
+        client.Server_connection_start(5000);
 
     }
 
+
+    public void Server_connection_start(int port) throws IOException {
+        String input = "";
+
+        Socket socket = new Socket("127.0.0.1", port);
+        //set up the input and output
+        Scanner my_input = new Scanner(System.in);
+        DataOutputStream my_output = new DataOutputStream(socket.getOutputStream());
+        DataInputStream message_from_server = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        //check if connection success
+        if (socket.isConnected()) {
+            System.out.println("connection success\n");
+
+            while (!input.equals("quit")) {
+                System.out.println(message_from_server.readUTF());
+                input = my_input.nextLine();
+                my_output.writeUTF(input);
+            }
+
+            //disconnected
+            System.out.println("Connection End!\nBye Bye");
+
+        } else {
+            System.out.println("connection fail");
+        }
+
+
+    }
 
     public void TGS_connection_start(int port) throws IOException {
         String input = "";
--- Assignment5/src/project/File_Server.java
+++ Assignment5/src/project/File_Server.java
@@ -1,8 +1,72 @@
 package project;
 
-public class File_Server {
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class File_Server implements Server{
     String id;
+
     public File_Server(){
 
+    }
+
+
+    @Override
+    public void connection_Server() throws IOException {
+        String key = null;
+        String cipher_text = null;
+        ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("waiting for the client");
+        Socket socket = serverSocket.accept();
+        System.out.println("Client Accept From TGS SERVER");
+
+
+        //get the system input stream
+        DataOutputStream send_to_client = new DataOutputStream(socket.getOutputStream());
+        DataInputStream input_from_client = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+
+        send_to_client.writeUTF("welcome to server\nPlease enter the Cipher text");
+        String Client_input = input_from_client.readUTF();
+
+        while (Client_input != null) {
+            cipher_text = Client_input;
+            break;
+        }
+        System.out.println(cipher_text);
+
+        send_to_client.writeUTF("Please enter the key");
+        while (Client_input != null) {
+            key = input_from_client.readUTF();
+            break;
+        }
+        System.out.println(key);
+
+        //Decryption
+        conventer cv = new conventer(cipher_text);
+        byte[] in_byte = cv.breaker();
+        Decrypt decrypt = new Decrypt();
+        decrypt.Decrypt(in_byte, key);
+
+        try {
+            while (socket.isConnected()) {
+                send_to_client.writeUTF("you can type [quit] now ");
+            }
+
+            //close the socket
+        } catch (Exception e) {
+
+        } finally {
+            socket.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File_Server file_server = new File_Server();
+        file_server.connection_Server();
     }
 }
