009ca55f07c6f1ab504129d5549245f2ca899fea
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-14 20:06:30 -0400

    update

--- Assignment6/src/Individuls/Server.java
+++ Assignment6/src/Individuls/Server.java
@@ -3,6 +3,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -19,9 +20,61 @@ public class Server {
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Server server = new Server();
-        server.to_AC();
+        //server.to_AC();
+        server.connection_client();
     }
 
+    public void connection_client() throws IOException {
+        // Create server Socket
+        ServerSocket ss = new ServerSocket(5000);
+
+        // connect it to client socket
+        Socket s = ss.accept();
+        System.out.println("Connection established");
+
+        // to send data to the client
+        PrintStream ps
+                = new PrintStream(s.getOutputStream());
+
+        // to read data coming from the client
+        BufferedReader br
+                = new BufferedReader(
+                new InputStreamReader(
+                        s.getInputStream()));
+
+        // to read data from the keyboard
+        BufferedReader kb
+                = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        // server executes continuously
+        while (true) {
+
+            String str, str1;
+
+            // repeat as long as the client
+            // does not send a null string
+
+            // read from client
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+                str1 = kb.readLine();
+                // send to client
+                ps.println(str1);
+            }
+
+            // close connection
+            ps.close();
+            br.close();
+            kb.close();
+            ss.close();
+            s.close();
+
+            // terminate application
+            System.exit(0);
+
+        } // end of while
+    }
     public Server() {
 
     }
